d3792167f43a797f51dc04c3561967e843d889a8
elias <elias.macip.1995@gmail.com>
2020-05-05 15:05:45 -0500

    INIT

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Open Academy',  # TODO
+    'version': '13.0.0.1.0',  # TODO
+    'author': 'HomebrewSoft',
+    'website': 'https://gitlab.com/HomebrewSoft/misc/open_academy',  # TODO
+    'depends': [  # TODO Check
+    ],
+    'data': [  # TODO Check
+        # security
+        # data
+        # reports
+        # views
+    ],
+    'external_dependencies': [  # TODO Check
+    ],
+}
